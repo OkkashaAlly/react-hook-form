94bda818a627a4fdd8a020f16e38b73d5e1f2d2e
OkkashaAlly <steamnature006@gmail.com>
2023-09-06 07:03:46 +0300

    4 - Managing Form State

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,25 @@
+"use client"
+
 import { useForm } from "react-hook-form";
 
 
 export default function Home() {
-  const form = useForm() 
+  const {register} = useForm() 
   
   return <div className="">
     <h1>YouTube form</h1>
     <form className="w-[40%] bg-slate-200 rounded p-8 mx-auto mt-20">
       <div className="flex flex-col gap-2 mb-3">
         <label htmlFor="name">Name: </label>
-        <input className="border border-slate-400 text-2xl rounded " type="text" id="name" name="name" />
+        <input className="border border-slate-400 text-2xl rounded " type="text" id="name" {...register('username')} />
       </div>
       <div className="flex flex-col gap-2 mb-3">
         <label htmlFor="email">Email: </label>
-        <input className="border border-slate-400 text-2xl rounded " type="text" id="email" name="email" />
+        <input className="border border-slate-400 text-2xl rounded " type="text" id="email" {...register('email')} />
       </div>
       <div className="flex flex-col gap-2 mb-3">
         <label htmlFor="channel">Channel: </label>
-        <input className="border border-slate-400 text-2xl rounded " type="text" id="channel" name="channel" />
+        <input className="border border-slate-400 text-2xl rounded " type="text" id="channel" {...register('channel')} />
       </div>
       <button className="rounded bg-slate-600 p-4 py-2 text-white">Submit</button>
 
