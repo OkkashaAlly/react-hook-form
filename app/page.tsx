78c4beddcf9c2fe8ba3d32a8c878031f9724f9c1
OkkashaAlly <steamnature006@gmail.com>
2023-09-06 06:58:34 +0300

    2 - Form setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <h1>Hello</h1>;
+  return <div className="">
+    <h1>YouTube form</h1>
+    <form className="w-[40%] bg-slate-200 rounded p-8 mx-auto mt-20">
+      <div className="flex flex-col gap-2 mb-3">
+        <label htmlFor="name">Name: </label>
+        <input className="border border-slate-400 text-2xl rounded " type="text" id="name" name="name" />
+      </div>
+      <div className="flex flex-col gap-2 mb-3">
+        <label htmlFor="email">Email: </label>
+        <input className="border border-slate-400 text-2xl rounded " type="text" id="email" name="email" />
+      </div>
+      <div className="flex flex-col gap-2 mb-3">
+        <label htmlFor="channel">Channel: </label>
+        <input className="border border-slate-400 text-2xl rounded " type="text" id="channel" name="channel" />
+      </div>
+      <button className="rounded bg-slate-600 p-4 py-2 text-white">Submit</button>
+
+    </form>
+  </div>;
 }
