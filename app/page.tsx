4205369e02c6a92f55242d992675ed66461f58ca
OkkashaAlly <steamnature006@gmail.com>
2023-09-07 06:16:44 +0300

    6 - Form State and Rerenders

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,17 @@
 import { useForm } from "react-hook-form";
 import {DevTool} from '@hookform/devtools'
 
+let renderCount = 0;
 
 export default function Home() {
-  const {register, control} = useForm() 
+  const {register, control, watch} = useForm() 
+
+  const name = watch('username')
+  
+  renderCount++
   
   return <div className="">
-    <h1>YouTube form</h1>
+    <h1>YouTube form({renderCount/2} - {name})</h1>
     <form className="w-[40%] bg-slate-200 rounded p-8 mx-auto mt-20">
       <div className="flex flex-col gap-2 mb-3">
         <label htmlFor="name">Name: </label>
