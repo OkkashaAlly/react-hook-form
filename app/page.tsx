5a67ddd789b5a593d7a02d35bde6041880527d19
OkkashaAlly <steamnature006@gmail.com>
2023-09-12 07:53:01 +0300

    28 - Manually Trigger Validations

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,7 @@ export default function Home() {
     getValues,
     setValue,
     reset,
+    trigger,
   } = useForm<FormValues>({
     defaultValues: async () => {
       const response = await fetch(
@@ -162,15 +163,15 @@ export default function Home() {
                 notBlackListed: fieldValue =>
                   !fieldValue.toLowerCase().endsWith("baddomain.com") ||
                   "This Email domain is not supported",
-                
-                emailAvaliable: async (fieldValue) => {
+
+                emailAvaliable: async fieldValue => {
                   const response = await fetch(
                     `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
                   );
                   const data = await response.json();
                   const foundUser = data.length > 0;
                   return !foundUser || "Email already taken";
-                }
+                },
               },
             })}
           />
@@ -319,6 +320,14 @@ export default function Home() {
             className="rounded bg-slate-600 p-4 py-2 text-white"
           >
             Set value
+          </button>
+          <button
+            onClick={() => trigger("channel")}
+            // onClick={()=> trigger()}
+            type="button"
+            className="rounded bg-slate-600 p-4 py-2 text-white"
+          >
+            Trigger validation
           </button>
         </div>
       </form>
